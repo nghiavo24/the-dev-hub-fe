6f2438f9527859873ed2f05b0bb6b79110c4278f
nghiavo24 <nghia.vo1452@gmail.com>
2022-12-27 11:39:39 -0600

    Added google sign in link

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 
 const App = () => {
+  const provider = new GoogleAuthProvider();
+  provider.addScope("https://www.googleapis.com/auth/contacts.readonly");
+  const auth = getAuth();
+
+  const signInwithGoogle = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+      });
+  }
   return (
-    <div className=''>App</div>
+    <div>
+      <button onClick={signInwithGoogle}>Google Sign In</button>
+    </div>
   )
 }
 
