eadc3cfa0fb8691a4d7c6f64a0b087bb6e0d66f4
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2022-12-28 13:27:38 -0500

    save

--- src/components/MainHub.js
+++ src/components/MainHub.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { Link, Route, Routes } from "react-router-dom";
 import PostingCreate from "./PostingCreate";
+import PostingUpdate from "./PostingUpdate";
+import PostingDelete from "./PostingDelete";
+import PostingDetails from "./PostingDetails";
 
 function MainHub(){
 
     return(
         <div>
             <h1>MainHub</h1>
-            <PostingCreate/>
+            <button><PostingCreate/></button>
+            <button><PostingUpdate/></button>
+            <button><PostingDetails/></button>
+            <button><PostingUpdate/></button>
+
         </div>
     )
 }
