01384c6a8748d00075244df3646e61ea4ac5054d
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2022-12-28 12:58:49 -0500

    add the posting delete route

---
+++ src/components/PostingDelete.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function PostingDelete() {
+    
+
+
+    return(
+        <div>
+            Here we will create the PostingDelete
+        </div>
+    )
+}
