0cb0ef8e384e24d3d3f5f23fb2ef59c74bfc31f0
mobrewer <morgiearan@gmail.com>
2022-12-28 11:24:14 -0700

    Added psuedocode to files to start off with

--- src/components/MyHub.js
+++ src/components/MyHub.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const MyHub = () => {
   return (
-    <div>MyHub</div>
+    <div className='myhub'>
+        <h1>MyHub</h1>
+        <div>
+            {/* link will be added here to take this to applicationcreate */}
+            <button>Create a job post</button>
+            {/* Link tag ends here */}
+        </div>
+    </div>
   )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>Navbar
+        {/* Add logo for the dev hub with img tag */}
+        <div>
+            {/* add links to different pages here with the Link tag */}
+        </div>
+    </div>
+    
   )
 }
 
