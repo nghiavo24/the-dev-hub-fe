e86f8cf1a27ac6c760545c7979f247348b298848
mobrewer <morgiearan@gmail.com>
2022-12-29 10:18:19 -0700

    Added links to navbar and some code to map for myhub

--- src/components/MyHub.js
+++ src/components/MyHub.js
@@ -1,16 +1,32 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
  
+const [allApplications, setAllApplications] = useState('')
+
+const navigate = useNavigate()
+
+const handleGet = async (id) => {
+    const res = await axios.get('https://localhost8080/thedevhub/applications' + id)
+    console.log(res);
+    navigate(0)
+}
+
 const MyHub = () => {
   return (
     <div className='myhub'>
         <h1>MyHub</h1>
+            <Link to='/application/add'>
+           <button> Create a job post</button>
+            </Link>
         <div>
-            {/* link will be added here to take this to applicationcreate */}
-            <button>Create a job post</button>
-            {/* Link tag ends here */}
-        </div>
-        <div>
-            {/* want to do a postings.map of some sort to be able to list out all the postings that have been created by this user. */}
+            <ul>
+                {allApplications.map((application, id) => {
+                    return <li key={id}>
+                        {application.title} - {application.company} - {application.applied} - {application.hiring_managaer} - {application.work_site} - {application.location} - {application.url}
+                    </li>
+                })}
+            </ul>
         </div>
     </div>
   )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,10 @@ const Navbar = () => {
         {/* Add logo for the dev hub with img tag */}
         <div>
             {/* add links to different pages here with the Link tag */}
+            <Link to= '/mainhub'>Main Hub</Link>
+            <Link to='/myhub'>My Hub</Link>
+            <Link to='application/add'>New Application</Link>
+            
         </div>
     </div>
     
