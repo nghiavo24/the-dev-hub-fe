a32083a19c7380ccd79c0d2873804c3e91d911da
nghiavo24 <nghia.vo1452@gmail.com>
2022-12-28 22:24:15 -0600

    Added axios and save state for Notes

--- src/components/ApplicationDetails.js
+++ src/components/ApplicationDetails.js
@@ -5,16 +5,24 @@ import { useParams } from 'react-router-dom';
 const ApplicationDetails = () => {
   const { id } = useParams();
   const[allApps, setAllApps] = useState();
+  const[allNotes, setAllNotes] = useState();
 
   const getAllApp = () => {
     axios.get(`url/application/${id}`)
     .then((res) => { setAllApps(res.data)})
   }
+
+  const getNotes = () => {
+    axios.get(`url/notes/${id}`)
+    .then((res) => {setAllNotes(res.data)})
+  }
 useEffect(() =>{
   getAllApp();
+  getNotes();
 }, [])
 
 if (allApps === undefined) return;
+
 
 return(
   <div>
