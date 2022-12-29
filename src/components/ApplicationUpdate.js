e39baf9067d255f8217aaf0e15b07187bd70d773
nghiavo24 <nghia.vo1452@gmail.com>
2022-12-28 23:13:06 -0600

    Added function to handle input

--- src/components/ApplicationUpdate.js
+++ src/components/ApplicationUpdate.js
@@ -1,20 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const ApplicationUpdate = () => {
-  
+    const[appUpdate, setAppUpdate] = useState({
+        title: "",
+        company: "",
+        applied: "",
+        hiring_manager: "",
+        compensation: "",
+        work_site: "",
+        location: "",
+        url:""
+    })
+    
+
+
+    const handleAppUpdate = (e) => {
+        e.preventDefault()
+        const appUpdateInput = {...appUpdate}
+        appUpdateInput[e.target.value] = e.target.value;
+        setAppUpdate(appUpdateInput);
+    }
   
     return (
     <div>
         <div>Update an Application</div>
         <form onSubmit={createNewApp}>
-        <input placeholder='Job title' name='title' value={newApp.title} onChange={handleAppCreate} required />
-        <input placeholder='Company' name='company' value={newApp.company} onChange={handleAppCreate} required />
-        <input placeholder='Date applied' name='applied' value={newApp.applied} onChange={handleAppCreate} />
-        <input placeholder='Name of recruiter/hiring manager' name='hiring_manager' value={newApp.hiring_manager} onChange={handleAppCreate} required />
-        <input placeholder='Compensation' name='compensation' value={newApp.compensation} onChange={handleAppCreate} />
-        <input placeholder='Remote/In-person/Hybrid' name='work_site' value={newApp.work_site} onChange={handleAppCreate}  />
-        <input placeholder='Job location' name='location' value={newApp.location} onChange={handleAppCreate} />
-        <input placeholder='Job URL' name='url' value={newApp.url} onChange={handleAppCreate} />
+        <input placeholder='Job title' name='title' value={appUpdate.title} onChange={handleAppCreate} required />
+        <input placeholder='Company' name='company' value={appUpdate.company} onChange={handleAppCreate} required />
+        <input placeholder='Date applied' name='applied' value={appUpdate.applied} onChange={handleAppCreate} />
+        <input placeholder='Name of recruiter/hiring manager' name='hiring_manager' value={appUpdate.hiring_manager} onChange={handleAppCreate} required />
+        <input placeholder='Compensation' name='compensation' value={appUpdate.compensation} onChange={handleAppCreate} />
+        <input placeholder='Remote/In-person/Hybrid' name='work_site' value={appUpdate.work_site} onChange={handleAppCreate}  />
+        <input placeholder='Job location' name='location' value={appUpdate.location} onChange={handleAppCreate} />
+        <input placeholder='Job URL' name='url' value={appUpdate.url} onChange={handleAppCreate} />
         <button>Submit</button>
         </form>
     </div>
