cd30987ab0c3b24d77454d501ea855ab5fa6bdaf
nghiavo24 <nghia.vo1452@gmail.com>
2022-12-28 22:57:09 -0600

    add axios call & function to delete an app and return to main page

--- src/components/ApplicationDetails.js
+++ src/components/ApplicationDetails.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState} from 'react'
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 const ApplicationDetails = () => {
+  const navigate = useNavigate();
   const { id } = useParams();
   const[allApps, setAllApps] = useState();
   const[allNotes, setAllNotes] = useState();
@@ -17,6 +18,10 @@ const ApplicationDetails = () => {
     .then((res) => {setAllNotes(res.data)})
   }
 
+  const deleteApp = () =>{
+    axios.delete(`url/application/delete/${id}`)
+    .then((res) => { navigate('/')})
+  }
   
 useEffect(() =>{
   getAllApp();
@@ -40,6 +45,8 @@ if (allNotes === undefined) return;
     axios.delete(`url/notes/delete/${noteId}`)
     .then((res) => {window.location.reload()})
   }
+
+
 return(
   <div>
     <div>
