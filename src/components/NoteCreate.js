c9e30790975507acf134325873f3efb36ea277aa
nghiavo24 <nghia.vo1452@gmail.com>
2022-12-28 23:39:38 -0600

    Added form

--- src/components/NoteCreate.js
+++ src/components/NoteCreate.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react';
+import axios from 'axios';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const NoteCreate = () => {
+    const[newNote, setNewNote] = useState({
+        content:""
+    })
+
+
+
   return (
-    <div>NoteCreate</div>
+    <div>
+        <div>Create a note</div>
+        <form>
+            <input placeholder='Content' name='content' value='' onChange='' required/>
+            <button>Submit</button>
+        </form>
+    </div>
   )
 }
 
