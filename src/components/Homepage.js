43fc211e7eefebcb6c130aa35c7f0cb8a828b93f
nghiavo24 <nghia.vo1452@gmail.com>
2022-12-29 23:41:24 -0600

    Homepage completed

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <div>
+        <div>Main Hub</div>
+        <Link to='/myhub'><div>Personal Hub</div></Link>
+    </div>
   )
 }
