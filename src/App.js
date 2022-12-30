cbb900c5659dfedf4d196b55ebe92c17774e8201
nghiavo24 <nghia.vo1452@gmail.com>
2022-12-29 23:38:24 -0600

    added function to require auth

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
 import Tasks from "./components/Tasks";
 import ApplicationDetails from './components/ApplicationDetails';
@@ -8,40 +8,11 @@ import Login from './components/Login';
 import SignUp from './components/SignUp';
 
 const App = () => {
-
-  const provider = new GoogleAuthProvider();
-  provider.addScope("https://www.googleapis.com/auth/contacts.readonly");
-  const auth = getAuth();
-
   const [authorizedUser,setAuthorizedUser] = useState(false || sessionStorage.getItem("accessToken"))
-
-  const signInwithGoogle = () => {
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        const token = credential.accessToken;
-        // The signed-in user info.
-        const user = result.user;
-        if(user){
-          user.getIdToken().then((tkn)=>{
-            // set access token in session storage
-            sessionStorage.setItem("accessToken", tkn);
-            setAuthorizedUser(true);
-          })
-        }
-        console.log(user);
-      })
-      .catch((error) => {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // The email of the user's account used.
-        const email = error.customData.email;
-        // The AuthCredential type that was used.
-        const credential = GoogleAuthProvider.credentialFromError(error);
-      });
-    }
+  
+  const requireAuth = ({children}) => {
+    return authorizedUser ? (children) : <Navigate to='/login' />
+  }
 
     const logoutUser = () => {
       signOut(auth).then(() => {      
@@ -66,11 +37,11 @@ const App = () => {
         </div>
       ): (
         <div>
-      <button onClick={signInwithGoogle}>SignWithGoogle</button>
+      
         </div>
       )}
       <Routes>
-        <Route path='/login' element={<Login />}/>
+        <Route path='/login' element={<Login setAuthorizedUser={setAuthorizedUser}/>}/>
         <Route path='/signup' element={<SignUp />}/>
       </Routes>
     </div>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,17 @@
 import React, { useState } from 'react'
 import {auth} from '../config/firebase-config'
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
 import {signInWithEmailAndPassword} from 'firebase/auth'
 import { useNavigate } from 'react-router-dom'
 
-const Login = () => {
+const Login = ({setAuthorizedUser}) => {
     const navigate = useNavigate();
     const[error, setError] = useState(false);
     const[email, setEmail] = useState('');
     const[password, setPassword] = useState('');
+    const provider = new GoogleAuthProvider();
+    provider.addScope("https://www.googleapis.com/auth/contacts.readonly");
+    const auth = getAuth();
 
 
     const handleLogin = (e) =>{
@@ -22,6 +26,34 @@ const Login = () => {
     });
     }
 
+    const signInwithGoogle = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        if(user){
+          user.getIdToken().then((tkn)=>{
+            // set access token in session storage
+            sessionStorage.setItem("accessToken", tkn);
+            setAuthorizedUser(true);
+          })
+        }
+        console.log(user);
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+      });
+    }
+
   return (
     <div>
         <form onSubmit={handleLogin}>
@@ -30,6 +62,7 @@ const Login = () => {
             {error && <span> Wrong email or password!</span>}
             <button type='submit'>Login</button>
         </form>
+        <button onClick={signInwithGoogle}>SignWithGoogle</button>
     </div>
   )
 }
