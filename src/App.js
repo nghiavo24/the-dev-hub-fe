5f9e89d65b219fc357f68c559f7a5fcffac1d835
nghiavo24 <nghia.vo1452@gmail.com>
2022-12-30 20:29:34 -0600

    Removed all auth related codes/files

--- src/App.js
+++ src/App.js
@@ -1,54 +1,17 @@
-import React, {useState} from 'react'
-import { Route, Routes, Navigate } from 'react-router-dom';
-import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
-import Tasks from "./components/Tasks";
-import ApplicationDetails from './components/ApplicationDetails';
-import ApplicationCreate from './components/ApplicationCreate';
-import Login from './components/Login';
-import SignUp from './components/SignUp';
-import Homepage from './components/Homepage'
- 
+import React from 'react'
+import { Route, Routes, } from 'react-router-dom';
+import MyHub from './components/MyHub';
+
 
 const App = () => {
-  const [authorizedUser,setAuthorizedUser] = useState(false || sessionStorage.getItem("accessToken"))
-  const isLoggedIn = false;
-  const RequireAuth = ({ children }) => {
-    return isLoggedIn ? children : <Navigate to='/login' />;
-  }
-
-    // const logoutUser = () => {
-    //   signOut(auth).then(() => {      
-    //     // clear session storage
-    //     sessionStorage.clear();
-    //     setAuthorizedUser(false);
-    //     // window.location.replace("/");
-    //     alert('Logged Out Successfully');
-    //   }).catch((error) => {
-    //     // An error happened.
-    //     alert(error);
-    //   });
-    // }
-  console.log(isLoggedIn)
+  
   return (
     <div>
-      <Routes>
-        <Route path='/'>
-          <Route index element={<RequireAuth><Homepage /></RequireAuth>} />
-          <Route path='login' element={<Login isLoggedIn={isLoggedIn} authorizedUser={authorizedUser}/>}/>
-        </Route>
-        <Route path='/signup' element={<SignUp />}/>
+        <Routes>
+          <Route path='/myhub' element={<MyHub />} />
       </Routes>
     </div>
   )
 }
 
 export default App
-
-// {authorizedUser ? (
-//   <div>
-//     <p>Authorized user</p>
-//     <h1>Tasks</h1>
-//     <Tasks token={sessionStorage.getItem("accessToken")}/>
-//     <button onClick={logoutUser}>Logout Button</button>
-//   </div>
-// ):
--- src/components/MyHub.js
+++ src/components/MyHub.js
@@ -1,19 +1,29 @@
 import axios from 'axios'
-import React, { useState } from 'react'
+import React, { useState, useEffect} from 'react'
 import { Link, useNavigate } from 'react-router-dom'
  
-const [allApplications, setAllApplications] = useState('')
 
-const navigate = useNavigate()
 
-const handleGet = async () => {
-    const res = await axios.get(`url`)
-    // adding links later /applications
-    console.log(res);
-    navigate(0)
-}
+const MyHub = ({token}) => {
+const [allApplications, setAllApplications] = useState()
 
-const MyHub = () => {
+const fetchData = async(token)=>{
+        const res = await axios.get('http://localhost:8080/thedevhub/application', {
+            headers:{
+                'Authorization': `Bearer ${token}`
+            }
+        })
+        setAllApplications(res.data)
+        console.log(res)
+    }
+console.log(allApplications)
+
+useEffect(()=>{
+    if(token){
+     fetchData(token);
+    }
+   },[]);
+
   return (
     <div className='myhub'>
         <h1>MyHub</h1>
@@ -27,7 +37,7 @@ const MyHub = () => {
                         {application.title} - {application.company} - {application.applied} - {application.hiring_managaer} - {application.work_site} - {application.location} - {application.url}
                     {/* want to add functionality to be able to click on each application and take you to that speciifc appluication. Connect to application details */}
                     </li>
-                })}
+                })} 
             </ul>
         </div>
     </div>
