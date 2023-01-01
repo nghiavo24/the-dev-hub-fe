c55f45679fd6de58ca7e959474318ad0d04307c3
nghiavo24 <nghia.vo1452@gmail.com>
2023-01-01 00:02:45 -0600

    Added route for NoteCreate

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Homepage from './components/Homepage';
 import MyHub from './components/MyHub';
 import MainHub from './components/MainHub';
 import ApplicationDetail from './components/ApplicationDetails'
+import NoteCreate from './components/NoteCreate';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
           <Route path='/mainhub' element={<MainHub />} />
           <Route path='/myhub' element={<MyHub />} />
           <Route path='/myhub/application/:id' element={<ApplicationDetail />} />
+          <Route path='/myhub/application/:id/note/add' element={<NoteCreate />}/>
       </Routes>
     </div>
   )
