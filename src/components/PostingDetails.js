f9ae3ce64083886db57cf11d5df5028ca477bee0
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-01-02 14:30:07 -0500

    add the front end aspect of the update functionlity

--- src/components/PostingDetails.js
+++ src/components/PostingDetails.js
@@ -8,7 +8,13 @@ export default function PostingDetails() {
     const { id } = useParams();
 
     const[postDetails, setPostDetails] = useState();
-
+    const [postUpdate, setPostUpdate] = useState({
+        title: "",
+        company: "",
+        posted: "",
+        url: "",
+        note: "",
+      });
     const getPostings = () => {
         axios.get(`https://the-dev-hub-app.herokuapp.com/api/thedevhub/posting/${id}`)
         .then(res => {
@@ -28,29 +34,60 @@ export default function PostingDetails() {
         .then((res) => (navigate('/mainhub')))
     }
 
-    const updatePost = async(e) => {
-        e.preventDefault()
+    async function updatingPost(e) {
+        e.preventDefault();
+        const editedPost = {
+          title: postUpdate.title,
+          company: postUpdate.company,
+          posted: postUpdate.posted,
+          url: postUpdate.url,
+          note: postUpdate.note,
+        };
+
         try{
-            await axios.put(`https://the-dev-hub-app.herokuapp.com/api/thedevhub/posting/update/${id}`, postDetails )
+            await axios.put(`https://the-dev-hub-app.herokuapp.com/api/thedevhub/posting/update/${id}`, editedPost )
             navigate('/mainhub')
         } catch(err){
             console.log(err)
         }
     }
 
-    // const individualPosting = getPostings.find(({_id}) => _id === {id})
+    const updateAPost = (e) => {
+        e.preventDefault();
+        const updatePostInput = {...postUpdate};
+        updatePostInput[e.target.name] = e.target.value;
+        setPostUpdate(updatePostInput);
+    }
 
 
 
 
     return(     
         <div>
-            <div>
+            <h1 class="p-4">Posting Details</h1>
+            <div class="flex container">
                 {postDetails.company}
+                {postDetails.title}
+                {postDetails.posted}
+                {postDetails.url}
+                {postDetails.note}
             </div>
+            <form class="p-10" updatingPost={updatingPost}>
+            <h3> Update this Posting</h3>
+                <label>Title</label>
+                <input type="text" name='title' value={postUpdate.title}  onChange={updateAPost} placeholder="add job title"/>
+                <label>Company Name</label>
+                <input type="text" name="company" value={postUpdate.company} onChange={updateAPost} placeholder="add company name" />
+                <label >Date Posted</label>
+                <input type="date" name="posted" value={postUpdate.posted} onChange={updateAPost} placeholder="date" />
+                <label>URL</label>
+                <input  type='url' name='url' value={postUpdate.url}  onChange={updateAPost} placeholder="url link"/>
+                <label>Note</label>
+                <input type="text" name="note" value={postUpdate.note} onChange={updateAPost} placeholder="notes" />
 
-                {/* <button onClick={updatePost}>Update Post</button> */}
-                <button onClick={deletePost}>Delete a Post</button>
+                <button class="p-4" onClick={updatingPost}>Update Post</button>
+                <button class="p-4" onClick={deletePost}>Delete a Post</button>
+            </form>    
         </div>
     )
 }
