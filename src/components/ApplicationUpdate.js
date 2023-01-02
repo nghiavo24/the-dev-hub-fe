48fd6beab876b6ecfb1c4a062429f4134de10cda
mobrewer <morgiearan@gmail.com>
2023-01-02 13:51:24 -0700

    Removed required to be able to update just one field and not have to update all

--- src/components/ApplicationUpdate.js
+++ src/components/ApplicationUpdate.js
@@ -29,7 +29,7 @@ const ApplicationUpdate = () => {
     const handleAppUpdate = (e) => {
         e.preventDefault()
         const appUpdateInput = {...appUpdate}
-        appUpdateInput[e.target.value] = e.target.value;
+        appUpdateInput[e.target.name] = e.target.value;
         setAppUpdate(appUpdateInput);
     }
     
@@ -37,10 +37,10 @@ const ApplicationUpdate = () => {
     <div>
         <div>Update an Application</div>
         <form onSubmit={appUpdateCall}>
-        <input placeholder='Job title' name='title' value={appUpdate.title} onChange={handleAppUpdate} required />
-        <input placeholder='Company' name='company' value={appUpdate.company} onChange={handleAppUpdate} required />
+        <input placeholder='Job title' name='title' value={appUpdate.title} onChange={handleAppUpdate}/>
+        <input placeholder='Company' name='company' value={appUpdate.company} onChange={handleAppUpdate}/>
         <input placeholder='Date applied' name='applied' value={appUpdate.applied} onChange={handleAppUpdate} />
-        <input placeholder='Name of recruiter/hiring manager' name='hiring_manager' value={appUpdate.hiring_manager} onChange={handleAppUpdate} required />
+        <input placeholder='Name of recruiter/hiring manager' name='hiring_manager' value={appUpdate.hiring_manager} onChange={handleAppUpdate}/>
         <input placeholder='Compensation' name='compensation' value={appUpdate.compensation} onChange={handleAppUpdate} />
         <input placeholder='Remote/In-person/Hybrid' name='work_site' value={appUpdate.work_site} onChange={handleAppUpdate}  />
         <input placeholder='Job location' name='location' value={appUpdate.location} onChange={handleAppUpdate} />
