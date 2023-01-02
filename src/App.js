fe304e0af2b35ae595d88d254656797637c2c98a
nghiavo24 <nghia.vo1452@gmail.com>
2023-01-01 23:14:42 -0600

    Added link to ApplicationCreate

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import NoteCreate from './components/NoteCreate';
 import Navbar from './components/Navbar';
 import PostingCreate from './components/PostingCreate';
 import ApplicationCreate from './components/ApplicationCreate';
+import ApplicationUpdate from './components/ApplicationUpdate';
 
 const App = () => {
   return (
@@ -16,10 +17,11 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Homepage />}/>
           <Route path='/mainhub' element={<MainHub />}/>
-          <Route path='/mainhub/posting/add' element={<PostingCreate />}/> 
+          <Route path='/mainhub/posting/create' element={<PostingCreate />}/> 
           <Route path='/myhub' element={<MyHub />} />
           <Route path='/myhub/application/:id' element={<ApplicationDetail />}/>
           <Route path='/myhub/application/create' element={<ApplicationCreate />}/>
+          <Route path='/myhub/application/:id/update' element={<ApplicationUpdate />}/>
           <Route path='/myhub/application/:id/note/add' element={<NoteCreate />}/>
       </Routes>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,19 +20,19 @@ const Navbar = () => {
               <a class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
             </li>
             <li>
-              <a class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"><Link to='/about' >About</Link></a>
+              <a class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"><Link to='/about'>About</Link></a>
             </li>
             <li>
-              <a class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"><Link to='/mainhub' >Main Hub</Link></a>
+              <a class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"><Link to='/mainhub'>Main Hub</Link></a>
             </li>
             <li>
-              <a class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"><Link to='/myhub' >My Hub</Link></a>
+              <a class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"><Link to='/myhub'>My Hub</Link></a>
             </li>
             <li>
-              <a class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"><Link to='/mainhub/posting/create' >New Position</Link></a>
+              <a class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"><Link to='/mainhub/posting/create'>New Position</Link></a>
             </li>
             <li>
-              <a class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">New Application</a>
+              <a class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"><Link to='/myhub/application/create'>New Application</Link></a>
             </li>
           </ul>
         </div>
