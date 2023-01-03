456d1a4e9f0d270aae9e38ca4e30658a423988b8
nghiavo24 <nghia.vo1452@gmail.com>
2023-01-02 22:35:41 -0600

    Homepage is completed

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import logo from '../media/homepagelogo.png'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import logo from '../media/homepagelogo.png';
+import { Link } from 'react-router-dom';
+
 
 const Homepage = () => {
   return (
@@ -10,11 +11,11 @@ const Homepage = () => {
             <div class="place-self-center mr-auto lg:col-span-7">
                 <h1 class="mb-4 max-w-2xl text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl dark:text-white">Organization tool for new software engineers</h1>
                 <p class="mb-6 max-w-2xl font-light text-gray-600 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">From searching to applying for new job, new software engineers use <strong>The Dev Hub</strong> to simplify and aid the job searching process.</p>
-                <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-                    Main Hub
+                <a class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-blue-500 hover:text-white focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                    <Link to='/mainhub'>Main Hub</Link>
                 </a>
-                <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-                    My Hub
+                <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-blue-500 hover:text-white focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                    <Link to='/myhub'>My Hub</Link>
                 </a> 
             </div>
             <div class="p-4 first-line:lg:mt-0 lg:col-span-5  lg:flex">
