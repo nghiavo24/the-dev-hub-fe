94ceb6ac79f455670624cdefa711cf0f44407c23
mobrewer <morgiearan@gmail.com>
2023-01-04 09:25:02 -0700

    Layout of new applciation is set, wanting to add fun images and things later

--- src/components/ApplicationCreate.js
+++ src/components/ApplicationCreate.js
@@ -1,10 +1,10 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router";
 
 const ApplicationCreate = () => {
     const navigate = useNavigate()
-    const[newApp, setNewApp] = useState({
+    const [newApp, setNewApp] = useState({
         title: "",
         company: "",
         applied: "",
@@ -12,97 +12,91 @@ const ApplicationCreate = () => {
         compensation: "",
         work_site: "",
         location: "",
-        url:""
+        url: ""
     })
 
     const createNewApp = async (e) => {
         e.preventDefault();
-        try{
+        try {
             await axios.post('https://the-dev-hub-app.herokuapp.com/api/thedevhub/application/create', newApp)
             navigate('/myhub')
-        } catch(err){
+        } catch (err) {
             console.log(err)
         }
     }
 
     const handleAppCreate = (e) => {
         e.preventDefault()
-        const newAppInput = {...newApp};
+        const newAppInput = { ...newApp };
         newAppInput[e.target.name] = e.target.value;
         setNewApp(newAppInput);
     }
 
     console.log(newApp)
-  return (
-    <div>
-        <div className='text-4xl text-center mx-44'>Create New Application</div>
-        <form onSubmit={createNewApp} className='mx-7 my-4 px-3 py-2'>
-            <input 
-                className='mx-7'
-                type='text' 
-                placeholder='Job title' 
-                name='title' value={newApp.title} 
-                onChange={handleAppCreate} required />
-            <br />
-            <input 
-                className='mx-7 my-3'
-                type='text' 
-                placeholder='Company' 
-                name='company' value={newApp.company} 
-                onChange={handleAppCreate} required />
-            <br />
-            <input 
-                className='mx-7'
-                type='date' 
-                placeholder='Date applied' 
-                name='applied' 
-                value={newApp.applied} 
-                onChange={handleAppCreate} />
-            <br />
-            <input 
-                className='mx-7 my-3'
-                type='text' 
-                placeholder='Name of recruiter/hiring manager' 
-                name='hiring_manager' value={newApp.hiring_manager} 
-                onChange={handleAppCreate}/>
-            <br />
-            <input 
-                className='mx-7'
-                type='text' 
-                placeholder='Compensation' 
-                name='compensation' 
-                value={newApp.compensation} 
-                onChange={handleAppCreate} />
-            <br />
-            <select 
-                className='mx-7 my-3'  
-                name='work_site' 
-                value={newApp.work_site}
-                onChange={handleAppCreate} > 
-                <option value="Remote">Remote</option>
-                <option value="In-person">In-person</option>
-                <option value="Hybrid">Hybrid</option>
-                
-            </select>
-            <br />
-            <input
-                className='mx-7'
-                type='text' 
-                placeholder='Job location' 
-                name='location' value={newApp.location} 
-                onChange={handleAppCreate} />
-            <br />
-            <input 
-                className='mx-7 my-3'
-                type='url' 
-                placeholder='Job URL' 
-                name='url' value={newApp.url} 
-                onChange={handleAppCreate} />
-            <br />
-            <button type="submit">Submit</button>
-        </form>
-    </div>
-  )
+    return (
+        <div>
+            <div className='text-4xl text-center mx-44'>Create New Application</div>
+            <div className="flex justify-center">
+                <form onSubmit={createNewApp} className='w-3/5 my-4 px-3 flex flex-col'>
+                    <input
+                        type='text'
+                        placeholder='Job title'
+                        name='title' value={newApp.title}
+                        onChange={handleAppCreate} required />
+                    <br />
+                    <input
+                        type='text'
+                        placeholder='Company'
+                        name='company' value={newApp.company}
+                        onChange={handleAppCreate} required />
+                    <br />
+                    <input
+                        type='date'
+                        placeholder='Date applied'
+                        name='applied'
+                        value={newApp.applied}
+                        onChange={handleAppCreate} />
+                    <br />
+                    <input
+                        type='text'
+                        placeholder='Name of recruiter/hiring manager'
+                        name='hiring_manager' value={newApp.hiring_manager}
+                        onChange={handleAppCreate} />
+                    <br />
+                    <input
+                        type='text'
+                        placeholder='Compensation'
+                        name='compensation'
+                        value={newApp.compensation}
+                        onChange={handleAppCreate} />
+                    <br />
+                    <select
+                        name='work_site'
+                        value={newApp.work_site}
+                        onChange={handleAppCreate} >
+                        <option value="Remote">Remote</option>
+                        <option value="In-person">In-person</option>
+                        <option value="Hybrid">Hybrid</option>
+
+                    </select>
+                    <br />
+                    <input
+                        type='text'
+                        placeholder='Job location'
+                        name='location' value={newApp.location}
+                        onChange={handleAppCreate} />
+                    <br />
+                    <input
+                        type='url'
+                        placeholder='Job URL'
+                        name='url' value={newApp.url}
+                        onChange={handleAppCreate} />
+                    <br />
+                    <button type="submit" className="rounded-lg text-lg mt-5 px-2 py-2 text-white tracking-wider bg-air-blue outline-none mx-10">Submit</button>
+                </form>
+            </div>
+        </div>
+    )
 }
 
 export default ApplicationCreate
