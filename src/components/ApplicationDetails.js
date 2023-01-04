0c04313616198d5c394f1d081971bb0ac1848b99
nghiavo24 <nghia.vo1452@gmail.com>
2023-01-04 10:23:04 -0600

    Added token to ApplicationDetail

--- src/components/ApplicationDetails.js
+++ src/components/ApplicationDetails.js
@@ -3,23 +3,36 @@ import axios from 'axios';
 import { useParams, useNavigate, Link} from 'react-router-dom';
 
 const ApplicationDetails = () => {
+  const token= sessionStorage.getItem("accessToken"); 
   const navigate = useNavigate();
   const { id } = useParams();
   const[allApps, setAllApps] = useState();
   const[allNotes, setAllNotes] = useState();
 
   const getAllApp = () => {
-    axios.get(`https://the-dev-hub-app.herokuapp.com/api/thedevhub/application/${id}`)
+    axios.get(`http://localhost:8080/api/thedevhub/application/${id}`, {
+      headers:{
+          'Authorization': `Bearer ${token}`
+      }
+  })
     .then((res) => {setAllApps(res.data)})
   }
 
   const getNotes = () => {
-    axios.get(`https://the-dev-hub-app.herokuapp.com/api/thedevhub/note/${id}`)
+    axios.get(`http://localhost:8080/api/thedevhub/note/${id}`, {
+      headers:{
+          'Authorization': `Bearer ${token}`
+      }
+  })
     .then((res) => {setAllNotes(res.data)})
   }
 
   const deleteApp = () =>{
-    axios.delete(`https://the-dev-hub-app.herokuapp.com/api/thedevhub/application/delete/${id}`)
+    axios.delete(`http://localhost:8080/api/thedevhub/application/delete/${id}`, {
+      headers:{
+          'Authorization': `Bearer ${token}`
+      }
+  })
     .then((res) => { navigate('/myhub')})
   }
 
@@ -44,7 +57,11 @@ if (allNotes === undefined) return;
   })
 
   const deleteNote = (noteId) => {
-    axios.delete(`https://the-dev-hub-app.herokuapp.com/api/thedevhub/note/delete/${noteId}`)
+    axios.delete(`http://localhost:8080/api/thedevhub/note/delete/${noteId}`, {
+      headers:{
+          'Authorization': `Bearer ${token}`
+      }
+  })
     .then((res) => {window.location.reload()})
   }
 
@@ -63,6 +80,9 @@ return(
         <Link to={`/myhub/application/${id}/update`}>
         Update
         </Link>
+      </button>
+      <button onClick={deleteApp}>
+        Delete
       </button>
         </div>
       <div className='w-1/4'>
