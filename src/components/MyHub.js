28e38ac73ac2568f37b19f8891d5845e53145680
mobrewer <morgiearan@gmail.com>
2023-01-03 17:00:22 -0700

    Got them centered and somewhat the same by using box-border but still needs work

--- src/components/MyHub.js
+++ src/components/MyHub.js
@@ -18,11 +18,11 @@ if (allApplications === undefined) return;
 
 const appList = allApplications.map((app, index) => {
     return(
-        <div key={index} className="p-6 border mx-auto my-5 border-blonde bg-white shadow-lg rounded-lg">
+        <div key={index} className="p-6 border mx-auto my-5 border-blonde bg-white shadow-lg rounded-lg box-border h-30 w-60">
             <div class="container grid grid-cols-2 gap-4">
             <p className="text-2xl font-bold text-dark-salmon">{app.company}</p>
             <p className="text-lg">{app.title}</p>
-            <p className="py-2">{app.applied}</p>
+            <p>{app.applied}</p>
             <Link to={`/myhub/application/${app._id}`}>
             <button className="bg-air-blue text-white group-hover:text-white rounded-lg py-2 px-4">View Details</button>
             </Link>
@@ -39,7 +39,7 @@ const appList = allApplications.map((app, index) => {
                 <span class="relative text-black group-hover:text-white">Create application</span>
             </button>
             </Link>
-           <div className="container grid grid-cols-2 gap-4">{appList}</div>
+           <div className="container grid grid-cols-2 gap-4 mx-auto">{appList}</div>
     
     </div>
   )
