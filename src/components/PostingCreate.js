0648b5d20e40d495403224f8cda9e96f60f02621
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-01-04 11:24:56 -0500

    work on the create postin fform to align with the page in center and look like appliations page

--- src/components/PostingCreate.js
+++ src/components/PostingCreate.js
@@ -35,19 +35,21 @@ export default function PostingCreate() {
     <div>
         <h3 className='text-4xl text-center mx-44' > Create a Posting</h3>
 
-        <form class="mx-10 my-10"  onSubmit={createNewPost}>
-                <input class=" border-black border mx-7" type="text"  name='title' value={post.title}  onChange={createAPost} placeholder="add job title"/>
+        <div class="flex justify-center ">
+        <form class="mx-4 my-4 px-3 flex flex-col w-3/5"  onSubmit={createNewPost}>
+                <input type="text"  name='title' value={post.title}  onChange={createAPost} placeholder="add job title"/>
                 <br/>
-                <input class=" border-black border mx-7 my-3 " type="text" name="company" value={post.company} onChange={createAPost} placeholder="add company name" />
+                <input  type="text" name="company" value={post.company} onChange={createAPost} placeholder="add company name" />
                 <br/>
-                <input class=" border-black border mx-7 "  type="date" name="posted" value={post.posted} onChange={createAPost} placeholder="date" />
+                <input   type="date" name="posted" value={post.posted} onChange={createAPost} placeholder="date" />
                 <br/>
-                <input class=" border-black border mx-7 my-3"   type='url' name='url' value={post.url}  onChange={createAPost} placeholder="url link"/>
+                <input  type='url' name='url' value={post.url}  onChange={createAPost} placeholder="url link"/>
                 <br/>
-                <input class=" border-black border mx-7 "  type="text" name="note" value={post.note} onChange={createAPost} placeholder="notes" />
+                <input   type="text" name="note" value={post.note} onChange={createAPost} placeholder="notes" />
                 <br/>
                 <button class="rounded-lg text-lg mt-5 px-2 py-2 text-white tracking-wider bg-air-blue outline-none mx-10"  type="submit"> Create Posting </button>
         </form>
+        </div>
       </div>
     )
 }
