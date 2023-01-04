771e31712707cde33cc34a1175e5e4f148aa60e8
mobrewer <morgiearan@gmail.com>
2023-01-04 10:13:04 -0700

    Added different border and shadow color, added margin to create app button

--- src/components/MyHub.js
+++ src/components/MyHub.js
@@ -18,7 +18,7 @@ if (allApplications === undefined) return;
 
 const appList = allApplications.map((app, index) => {
     return(
-        <div key={index} className="container gap-4 p-5 border text-center my-5 border-blonde bg-white shadow-lg rounded-lg">
+        <div key={index} className="container gap-4 p-5 border text-center my-5 border-gray-300 bg-white shadow-lg shadow-air-blue rounded-lg">
             <div class="p-5">
             <p className="text-2xl font-bold text-dark-salmon">{app.company}</p>
             <p className="text-lg my-4">{app.title}</p>
@@ -34,12 +34,12 @@ const appList = allApplications.map((app, index) => {
     <div>
         <h1 className='text-4xl text-center mx-44'>MyHub</h1>
             <Link to='/myhub/application/create'>
-            <button class="group relative h-12 w-48 overflow-hidden rounded-lg bg-white text-lg shadow">
+            <button class="group relative h-12 w-48 overflow-hidden rounded-lg bg-white text-lg shadow mx-14">
                 <div class="absolute inset-0 w-3 bg-paolo-green transition-all duration-[800ms] ease-out group-hover:w-full"></div>
                 <span class="relative text-black group-hover:text-white">Create application</span>
             </button>
             </Link>
-           <div className="container grid grid-cols-1 gap-4 mx-auto py-9 md:grid-cols-2 lg:grid-cols-4">{appList}</div>
+           <div className="container grid grid-cols-1 gap-4 mx-auto py-9 justify-around md:grid-cols-2 lg:grid-cols-4">{appList}</div>
     
     </div>
   )
