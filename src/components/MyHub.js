762a27b3841fc6f6a40a903bfb0bfc57025a92c5
mobrewer <morgiearan@gmail.com>
2023-01-05 08:51:32 -0700

    Changed code to title to make title responsive

--- src/components/MyHub.js
+++ src/components/MyHub.js
@@ -47,7 +47,7 @@ const MyHub = ({ uid }) => {
 
   return (
     <div>
-        <h1 className='font-lora text-4xl text-center mx-44'>My Hub</h1>
+        <h1 className='font-lora flex justify-center text-4xl'>My Hub</h1>
            <div className="container grid grid-cols-1 gap-4 mx-auto px-5 py-9 justify-around md:grid-cols-2 lg:grid-cols-4">{displayList}</div>
     </div>
   )
