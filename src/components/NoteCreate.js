0b644233cecb6bf5c789d33733c64b60696d07a6
nghiavo24 <nghia.vo1452@gmail.com>
2023-01-04 22:09:21 -0600

    Added font in NoteCreat, added on-going issue to Trello

--- src/components/NoteCreate.js
+++ src/components/NoteCreate.js
@@ -34,11 +34,11 @@ const NoteCreate = () => {
 
     return (
         <div>
-            <div className='text-4xl text-center mx-44'>Create a note</div>
+            <div className='font-lora text-4xl text-center mx-44'>Add Note</div>
             <div className="flex justify-center">
-                <form onSubmit={createNewNote} className='w-1/2 my-4 px-3 flex flex-col bg-white shadow-lg shadow-air-blue rounded-lg border-gray-300 border'>
+                <form onSubmit={createNewNote} className='font-montserrat w-1/2 my-4 px-3 flex flex-col bg-white shadow-lg shadow-air-blue rounded-lg border-gray-300 border'>
                     <input
-                        className="mt-5"
+                        className="mt-5 caret-pink-500"
                         type='text'
                         placeholder='Content'
                         name='content'
@@ -46,8 +46,8 @@ const NoteCreate = () => {
                         onChange={handleInput}
                         required />
                     <br />
-                    <button className="rounded-lg text-lg px-2 py-2 text-white tracking-wider bg-paolo-green outline-none mx-44"><Link to={`/myhub/application/${id}`}>Back</Link></button>
-                    <button className="rounded-lg text-lg my-5 px-2 py-2 text-white tracking-wider bg-air-blue outline-none mx-44">Submit</button>
+                    <button className="font-lobster rounded-lg text-lg my-5 px-4 py-4 text-white tracking-wider bg-paolo-green outline-none mx-44"><Link to={`/myhub/application/${id}`}>Back</Link></button>
+                    <button className="font-lobster rounded-lg text-lg my-5 px-4 py-4 text-white tracking-wider bg-air-blue outline-none mx-44">Submit</button>
                 </form>
             </div>
         </div>
