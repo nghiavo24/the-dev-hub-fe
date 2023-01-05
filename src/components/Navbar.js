dc4937156098ef3a4f83dc8b5b79d009c200d747
nghiavo24 <nghia.vo1452@gmail.com>
2023-01-05 00:09:16 -0600

    Display the user name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -36,7 +36,7 @@ const Navbar = ({authorizedUser, logOutUser, signInWithGoogle, displayName}) => 
             </li>
             {authorizedUser ? (
                 <div>
-                  <p>{displayName}</p>
+                  <p>Hi, {displayName}</p>
                   <button onClick={logOutUser}
                   type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
                     Sign Out
