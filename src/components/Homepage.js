54af815c8acdedc37ea402e1787e4240a5c4dfe9
nghiavo24 <nghia.vo1452@gmail.com>
2023-01-04 20:46:36 -0600

    Added fonts Lora, Lobster, Montaga, Montserrat

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -9,12 +9,12 @@ const Homepage = () => {
       <section class="bg-white dark:bg-gray-900">
         <div class="grid py-8 px-4 mx-auto max-w-screen-xl lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
             <div class="place-self-center mr-auto lg:col-span-7">
-                <h1 class="mb-4 max-w-2xl text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl dark:text-white">Organization tool for new software engineers</h1>
-                <p class="mb-6 max-w-2xl font-light text-gray-600 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">From searching to applying for new job, new software engineers use <strong>The Dev Hub</strong> to simplify and aid the job searching process.</p>
+                <h1 class="font-lora mb-4 max-w-2xl text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl dark:text-white">Organization tool for new software engineers</h1>
+                <p class="font-montserrat mb-6 max-w-2xl font-light text-gray-600 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">From searching to applying for new job, new software engineers use <span className='font-bold'>The Dev Hub</span> to simplify and aid the job searching process.</p>
                 <a class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-blue-500 hover:text-white focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
                     <Link to='/mainhub'>Main Hub</Link>
                 </a>
-                <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-blue-500 hover:text-white focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                <a class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-blue-500 hover:text-white focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
                     <Link to='/myhub'>My Hub</Link>
                 </a> 
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,22 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
-    colors: {
-      'dark-salmon': '#E89475',
-      'air-blue': '#7EA0B7',
-      'yellow-crayola': '#FFEF9F',
-      'paolo-green': '#409D78',
-      'blonde': '#F4F0BB',
-      'off-white': '#FAF9F6'
-    }
+    extend: {
+      colors: {
+        'dark-salmon': '#E89475',
+        'air-blue': '#7EA0B7',
+        'yellow-crayola': '#FFEF9F',
+        'paolo-green': '#409D78',
+        'blonde': '#F4F0BB',
+        'off-white': '#FAF9F6'
+      },
+    },
+    fontFamily: {
+      lora: ["Lora", "serif"],
+      lobster: ["Lobster", "cursive"],
+      montaga: ["Montaga", "serif"],
+      montserrat: ["Montserrat", "sans-serif"],
+    },
   },
   plugins: [
     require('flowbite/plugin')
