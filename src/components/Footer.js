bf8698b8ca292d2a94e7d2fa7857de41400ad836
nghiavo24 <nghia.vo1452@gmail.com>
2023-01-04 23:44:04 -0600

    Added some styling to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,33 +7,18 @@ const Footer = () => {
     <div className="font-montserrat p-4 bg-white md:p-8 lg:p-10 dark:bg-gray-800">
         <div className="mx-auto max-w-screen-xl text-center">
             <div href="#" className="flex justify-center items-center text-2xl font-semibold text-gray-900 dark:text-white">
-                <img className="mr-2 h-16" src={logo} />    
+                <img className="mr-2 h-20" src={logo} />    
             </div>
             <p className="my-6 text-gray-500 dark:text-gray-400">Open-source web application to facilitate job searching process for new fellow in Technology field.</p>
-            <ul className="flex flex-wrap justify-center items-center mb-6 text-gray-900 dark:text-white">
+            <ul className="font-lora flex flex-wrap justify-center items-center mb-6 text-gray-900 dark:text-white">
                 <li>
                     <div className="mr-4 hover:underline md:mr-6 "><Link to='/'>Home</Link></div>
                 </li>
                 <li>
                     <div className="mr-4 hover:underline md:mr-6"><Link to='/about'>About</Link></div>
                 </li>
-                <li>
-                    <div href="#" className="mr-4 hover:underline md:mr-6 ">Campaigns</div>
-                </li>
-                <li>
-                    <div href="#" className="mr-4 hover:underline md:mr-6">Blog</div>
-                </li>
-                <li>
-                    <div href="#" className="mr-4 hover:underline md:mr-6">Affiliate Program</div>
-                </li>
-                <li>
-                    <div href="#" className="mr-4 hover:underline md:mr-6">FAQs</div>
-                </li>
-                <li>
-                    <div href="#" className="mr-4 hover:underline md:mr-6">Contact</div>
-                </li>
             </ul>
-            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023-2024 <a href="#" class="hover:underline">The Dev Hub™</a>. All Rights Reserved.</span>
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023-2024 <span href="#" class="hover:underline">The Dev Hub™</span>. All Rights Reserved.</span>
         </div>
     </div>
   )
