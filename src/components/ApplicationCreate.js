75a96972ba143655f06c5156d611b7dc64b90366
nghiavo24 <nghia.vo1452@gmail.com>
2023-01-05 20:04:59 -0600

    Fixed styling for ApplicationCreate

--- src/components/ApplicationCreate.js
+++ src/components/ApplicationCreate.js
@@ -49,7 +49,7 @@ const ApplicationCreate = ({uid}) => {
 
     return (
         <div>
-            <div className='font-lora flex justify-center text-4xl overflow-hidden text-center'>Create New Application</div>
+            <div className='font-lora flex justify-center text-4xl overflow-hidden text-center'>New Application</div>
             <div className="flex justify-center">
                 <form onSubmit={createNewApp} className='font-montserrat w-3/4 md:w-1/2 lg:w-1/3 my-4 px-3 flex flex-col bg-white shadow-lg shadow-air-blue rounded-lg border-gray-300 border'>
                     <input
@@ -108,7 +108,7 @@ const ApplicationCreate = ({uid}) => {
                         onChange={handleAppCreate} />
                     <br />
                     <div className="flex justify-center">
-                        <button type="submit" className="font-lobster rounded-lg text-lg my-5 px-6 py-2 text-white tracking-wider bg-air-blue outline-none mx-10">Submit</button>
+                        <button type="submit" className="hover:bg-paolo-green font-lobster rounded-lg text-lg my-5 px-6 py-2 text-white tracking-wider bg-air-blue outline-none mx-10">Submit</button>
                     </div>
                 </form>
             </div>
